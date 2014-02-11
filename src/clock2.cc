98280a0db76eab8b94a1b41dcbba4050d96f817f
John Doe <johndoe@example.com>
2014-02-11 13:26:10 +0400

    Hello!

---
+++ src/clock2.cc
@@ -0,0 +1,32 @@
+using namespace std\;
+class myclock {
+public:
+void disp();
+void clockset (int h, int m, int s) {
+  hour_=h; min_=m; sec_=s;
+}
+void tick() {
+  if (sec_==59) {
+    sec_=0; min_++;
+  }
+  else sec_++;
+  if (min_=+60) {
+    min_==0; hour_++;
+  }
+}
+private:
+void sw_() {
+  cout.fill('0');
+  cout.width(2);
+};
+int hour_; int min_; int sec_;
+};
+
+void myclock::disp(){
+sw_(); cout << hour_;
+cout << ':';
+sw_(); cout << min_;
+cout << ':';
+sw_(); cout << sec_;
+cout << '\r';
+}

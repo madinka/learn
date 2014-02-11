aed02b40495cc21f04489bb0ea9b249089e37b2f
John Doe <johndoe@example.com>
2014-02-11 15:34:16 +0400

    python project

--- scripts/on.py
+++ scripts/on.py
@@ -1,6 +1,29 @@
 # coding: utf-8
 
+"""
 detector.upOutPower()
 
 for i in range(8):
-	detector.upRefPower()
+    detector.upRefPower()
+"""
+
+
+class MyStateClock(object):
+    def __init__(self, minutes):
+        self.min_ = minutes
+
+    def reset(self):
+        print self.min_
+
+
+def main():
+    clock = MyStateClock(9)
+
+    clock.reset()
+    print 0
+
+
+if __name__ == '__main__':
+    main()
+
+

126e1af0ae0ea900054bd3587ea0064a3901b75b
zaqwes <igor.a.lugansky@gmail.com>
2014-02-20 11:02:44 +0300

    save and push

--- src/std/std_test.cc
+++ src/std/std_test.cc
@@ -57,7 +57,42 @@ BOOST_PYTHON_MODULE(mymodule)
 
 
 TEST(Std, Vector) {
+  Py_Initialize();
+  try {
+    PyRun_SimpleString(
+      "a_foo = None\n"
+      "\n"
+      "def setup(a_foo_from_cxx):\n"
+      "  print 'setup called with', a_foo_from_cxx\n"
+      "  global a_foo\n"
+      "  a_foo = a_foo_from_cxx\n"
+      "\n"
+      "def run():\n"
+      "  a_foo.SetChannel(6)\n"
+      "  pass#a_foo.SetChannel(6)\n"
+      "\n"
+      "print 'main module loaded'\n"
+    );
 
+    // Not work...
+    shared_ptr<Preamplifier> fake(new PreamplifierImplFake);
+    boost::shared_ptr<PreamplifierWrapper> ptr_cc_object(
+        new PreamplifierWrapper(fake));
 
+    initpreampl_wrapper();
+    object main = object(handle<>(borrowed(PyImport_AddModule("__main__"))));
+
+    // pass the reference to a_cxx_foo into python:
+    object setup_function = main.attr("setup");
+    setup_function(ptr_cc_object);
+
+    // now run the python 'main' function
+    object run_func = main.attr("run");
+    run_func();
+  } catch (error_already_set) {
+    PyErr_Print();
+  }
+
+  Py_Finalize();
 }
 
